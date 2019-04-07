80ee57cfbae4f3a73fa3e745c1a2cd36e887592d
dmartynov <denis.martynov.93@mail.ru>
2019-04-07 19:04:02 +0700

    add static data to git, delete api key

---
+++ src/static/data/articles.js
@@ -0,0 +1,44 @@
+const articles = [
+    {
+        id: 1,
+        title: 'React vs Vue',
+        slug: 'react-vs-vue',
+        subtitle: 'Реакт остается лидером на мировой фронтенд арене JS фреймворков, но Вью уже наступает на пятки.',
+        annotation: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda corporis dignissimos ducimus eligendi error excepturi id incidunt, ipsam itaque magnam nemo nesciunt non odit quidem quis, recusandae saepe vel veritatis!',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, commodi cupiditate dicta error ex in inventore itaque iure, magni nesciunt, nulla officiis provident quia rerum sit. A ad corporis, earum ex excepturi laboriosam numquam, perspiciatis placeat quaerat quo sequi voluptas voluptate. Et fuga fugit laborum sit. Iure veniam vitae voluptatem.',
+        categoriesIds: [1, 2, 3],
+        url: 'https://martynov.ru/articles/react-vs-vue',
+        createdAt: '2019-04-06 18:12:10',
+        updatedAt: '2019-04-06 18:12:10',
+        author: {
+            id: 1,
+            name: 'Денис Мартынов',
+        },
+        views: 150,
+        likes: 10,
+        dislikes: 1,
+        comments: [],
+    },
+    {
+        id: 2,
+        title: 'Canvas простым языком',
+        slug: 'simple-canvas',
+        subtitle: 'Реакт остается лидером на мировой фронтенд арене JS фреймворков, но Вью уже наступает на пятки.',
+        annotation: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda corporis dignissimos ducimus eligendi error excepturi id incidunt, ipsam itaque magnam nemo nesciunt non odit quidem quis, recusandae saepe vel veritatis!',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, commodi cupiditate dicta error ex in inventore itaque iure, magni nesciunt, nulla officiis provident quia rerum sit. A ad corporis, earum ex excepturi laboriosam numquam, perspiciatis placeat quaerat quo sequi voluptas voluptate. Et fuga fugit laborum sit. Iure veniam vitae voluptatem.',
+        categoriesIds: [1, 2, 3],
+        url: 'https://martynov.ru/articles/simple-canvas',
+        createdAt: '2017-01-14 18:12:10',
+        updatedAt: '2017-01-14 18:12:10',
+        author: {
+            id: 1,
+            name: 'Денис Мартынов',
+        },
+        views: 99,
+        likes: 5,
+        dislikes: 2,
+        comments: [],
+    },
+];
+
+export default articles;
