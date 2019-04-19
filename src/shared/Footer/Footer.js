415c23f8f466ea4d03a034af9f27aee76447fa00
dmartynov <denis.martynov.93@mail.ru>
2019-04-19 18:53:57 +0700

    upd Footer styles

--- src/shared/Footer/Footer.js
+++ src/shared/Footer/Footer.js
@@ -13,40 +13,42 @@ export default class Footer extends React.PureComponent {
     render() {
         return (
             <footer className={bem.block()}>
-                <div className={bem.element('top')}>
-                    <div className={bem.element('offer')}>
-                        {__('Поделитесь своим опытом взаимодействия с сайтами')}
-                    </div>
-                    <Button
-                        label={__('Поделиться')}
-                        link
-                        url='1221'
-                    />
-                </div>
-                <div className={bem.element('bottom')}>
-                    <div className={bem.element('copyrights')}>
-                        <div className={bem.element('site-created-date')}>
-                            {__('Сделано с {heart} в 2019 г.', {
-                                heart: <Icon
-                                    className={bem.element('heart-icon')}
-                                    name='heart'
-                                    fill='#E53935'
-                                />
-                            })}
+                <div className={bem.element('inner')}>
+                    <div className={bem.element('top')}>
+                        <div className={bem.element('offer')}>
+                            {__('Поделитесь своим опытом взаимодействия с сайтами')}
                         </div>
-                        <div className={bem.element('design')}>
-                            {__('Дизайн')}&nbsp;
-                            <a
-                                className={bem.element('design-link')}
-                                href='https://design.chulakov.ru/'
-                                target='_blank'
-                            >
-                                Chulakov Design System
-                            </a>
-                        </div>
+                        <Button
+                            label={__('Поделиться')}
+                            link
+                            url='1221'
+                        />
                     </div>
-                    <div className={bem.element('socials')}>
-                        <SocialNetworks color='white'/>
+                    <div className={bem.element('bottom')}>
+                        <div className={bem.element('copyrights')}>
+                            <div className={bem.element('site-created-date')}>
+                                {__('Сделано с {heart} в 2019 г.', {
+                                    heart: <Icon
+                                        className={bem.element('heart-icon')}
+                                        name='heart'
+                                        fill='#E53935'
+                                    />
+                                })}
+                            </div>
+                            <div className={bem.element('design')}>
+                                {__('Дизайн')}&nbsp;
+                                <a
+                                    className={bem.element('design-link')}
+                                    href='https://design.chulakov.ru/'
+                                    target='_blank'
+                                >
+                                    Chulakov Design System
+                                </a>
+                            </div>
+                        </div>
+                        <div className={bem.element('socials')}>
+                            <SocialNetworks color='white'/>
+                        </div>
                     </div>
                 </div>
             </footer>
