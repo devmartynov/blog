6f2e87cd28a7b20b021c756c68e11f3e13badb17
dmartynov <denis.martynov.93@mail.ru>
2019-04-19 16:00:33 +0700

    upd static data

--- src/static/data/articles.js
+++ src/static/data/articles.js
@@ -1,10 +1,10 @@
-const articles = [
+export const articlesDev = [
     {
         id: 1,
         title: 'React vs Vue',
         slug: 'react-vs-vue',
         subtitle: 'Реакт остается лидером на мировой фронтенд арене JS фреймворков, но Вью уже наступает на пятки.',
-        annotation: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda corporis dignissimos ducimus eligendi error excepturi id incidunt, ipsam itaque magnam nemo nesciunt non odit quidem quis, recusandae saepe vel veritatis!',
+        annotation: 'Женщины, которые подходят для данной позиции, зачастую, стабильнее любого мужчины. Не брать женщину из-за «риска» беременности — абсолютная тупость. Потому что такой «риск» — это один из ста «рисков» любого мужика.',
         content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, commodi cupiditate dicta error ex in inventore itaque iure, magni nesciunt, nulla officiis provident quia rerum sit. A ad corporis, earum ex excepturi laboriosam numquam, perspiciatis placeat quaerat quo sequi voluptas voluptate. Et fuga fugit laborum sit. Iure veniam vitae voluptatem.',
         categoriesIds: [1, 2, 3],
         url: 'https://martynov.ru/articles/react-vs-vue',
@@ -24,7 +24,7 @@ const articles = [
         title: 'Canvas простым языком',
         slug: 'simple-canvas',
         subtitle: 'Реакт остается лидером на мировой фронтенд арене JS фреймворков, но Вью уже наступает на пятки.',
-        annotation: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda corporis dignissimos ducimus eligendi error excepturi id incidunt, ipsam itaque magnam nemo nesciunt non odit quidem quis, recusandae saepe vel veritatis!',
+        annotation: 'Женщины, которые подходят для данной позиции, зачастую, стабильнее любого мужчины. Не брать женщину из-за «риска» беременности — абсолютная тупость. Потому что такой «риск» — это один из ста «рисков» любого мужика.',
         content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, commodi cupiditate dicta error ex in inventore itaque iure, magni nesciunt, nulla officiis provident quia rerum sit. A ad corporis, earum ex excepturi laboriosam numquam, perspiciatis placeat quaerat quo sequi voluptas voluptate. Et fuga fugit laborum sit. Iure veniam vitae voluptatem.',
         categoriesIds: [1, 2, 3],
         url: 'https://martynov.ru/articles/simple-canvas',
@@ -41,4 +41,46 @@ const articles = [
     },
 ];
 
-export default articles;
+export const articlesLife = [
+    {
+        id: 1,
+        title: 'Полюбить себя',
+        slug: 'polybi-sebya',
+        subtitle: 'Нужно полюбить себя таким какой ты есть.',
+        annotation: 'Женщины, которые подходят для данной позиции, зачастую, стабильнее любого мужчины. Не брать женщину из-за «риска» беременности — абсолютная тупость. Потому что такой «риск» — это один из ста «рисков» любого мужика.',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, commodi cupiditate dicta error ex in inventore itaque iure, magni nesciunt, nulla officiis provident quia rerum sit. A ad corporis, earum ex excepturi laboriosam numquam, perspiciatis placeat quaerat quo sequi voluptas voluptate. Et fuga fugit laborum sit. Iure veniam vitae voluptatem.',
+        categoriesIds: [1, 2, 3],
+        url: 'https://martynov.ru/articles/react-vs-vue',
+        createdAt: '2019-04-06 18:12:10',
+        updatedAt: '2019-04-06 18:12:10',
+        author: {
+            id: 1,
+            name: 'Денис Мартынов',
+        },
+        views: 150,
+        likes: 10,
+        dislikes: 1,
+        comments: [],
+    },
+    {
+        id: 2,
+        title: '3 важных принципы',
+        slug: '3-vazhnih-principa',
+        subtitle: 'Нужно полюбить себя таким какой ты есть.',
+        annotation: 'Женщины, которые подходят для данной позиции, зачастую, стабильнее любого мужчины. Не брать женщину из-за «риска» беременности — абсолютная тупость. Потому что такой «риск» — это один из ста «рисков» любого мужика.',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, commodi cupiditate dicta error ex in inventore itaque iure, magni nesciunt, nulla officiis provident quia rerum sit. A ad corporis, earum ex excepturi laboriosam numquam, perspiciatis placeat quaerat quo sequi voluptas voluptate. Et fuga fugit laborum sit. Iure veniam vitae voluptatem.',
+        categoriesIds: [1, 2, 3],
+        url: 'https://martynov.ru/articles/simple-canvas',
+        createdAt: '2017-01-14 18:12:10',
+        updatedAt: '2017-01-14 18:12:10',
+        author: {
+            id: 1,
+            name: 'Денис Мартынов',
+        },
+        views: 99,
+        likes: 5,
+        dislikes: 2,
+        comments: [],
+    },
+];
+
